094aa3c8315e4ae4f3e0b07d66fcf755d14dceef
Sean Anderson <sean@seananderson.ca>
2015-03-21 23:53:51 -0700

    Tweak formatting

--- R/cmsy.R
+++ R/cmsy.R
@@ -39,14 +39,15 @@ cmsy <- function(
   ct,
   prior_log_mean,
   prior_log_sd,
-  interyr_index = 2,
-  interbio = c(0, 1),
-  bio_step = 0.05,
-  resilience = c(NA, "Very low", "Low", "Medium", "High"),
-  start_k = c(max(ct), 50 * max(ct)),
-  startbio = if (ct[1] / max(ct) < 0.2) c(0.5, 0.9) else c(0.2, 0.6),
-  sig_r = 0.05,
-  reps = 1e4) {
+  interyr_index    = 2L,
+  interbio         = c(0, 1),
+  bio_step         = 0.05,
+  resilience       = c(NA, "Very low", "Low", "Medium", "High"),
+  start_k          = c(max(ct), 50 * max(ct)),
+  startbio         = if (ct[1] / max(ct) < 0.2) c(0.5, 0.9) else c(0.2, 0.6),
+  sig_r            = 0.05,
+  reps             = 1e4) {
+
   if (!identical(length(interbio), 2L))
     stop("interbio must be a vector of length 2")
   if (!identical(length(start_k), 2L))
@@ -55,21 +56,22 @@ cmsy <- function(
     stop("yr and ct must be the same length")
 
   start_r <- switch(resilience[1],
-    "Very low" = c(0.015, 0.1),
-    "Low"      = c(0.050, 0.5),
-    "Medium"   = c(0.200, 1.0),
-    "High"     = c(0.600, 1.5),
-    "NA"       = c(0.015, 1.5))
+    "Very low"     = c(0.015, 0.1),
+    "Low"          = c(0.050, 0.5),
+    "Medium"       = c(0.200, 1.0),
+    "High"         = c(0.600, 1.5),
+    "NA"           = c(0.015, 1.5))
 
-  schaefer_cmsy(r_lim = start_r,
-    k_lim = start_k,
-    sig_r = sig_r,
-    startbt = seq(startbio[1], startbio[2], by = bio_step),
-    yr = yr,
-    ct = ct,
-    interyr_index = interyr_index,
+  schaefer_cmsy(
+    r_lim          = start_r,
+    k_lim          = start_k,
+    sig_r          = sig_r,
+    startbt        = seq(startbio[1], startbio[2], by = bio_step),
+    yr             = yr,
+    ct             = ct,
+    interyr_index  = interyr_index,
     prior_log_mean = prior_log_mean,
-    prior_log_sd = prior_log_sd,
-    interbio_lim = interbio,
-    reps = reps)
+    prior_log_sd   = prior_log_sd,
+    interbio_lim   = interbio,
+    reps           = reps)
 }
