041ffcbdfa070b6765a9ac683190a41f7b650511
Sean Anderson <sean@seananderson.ca>
2015-03-25 15:48:09 -0700

    Update CMSY example

--- R/cmsy.R
+++ R/cmsy.R
@@ -42,20 +42,16 @@ NULL
 #' # An example using cmsy() with a stock from the RAM Legacy database:
 #' # The stock is "BGRDRSE" (Blue Grenadier Southeast Australia)
 #'
-#' d <- structure(list(
-#'   yr = 1979:2011,
-#'   ct = c(512, 865, 511, 829, 935, 1390, 1260, 2240, 3370, 2770, 3050, 3290,
-#'     4540, 3300, 3500, 3190, 2880, 3490, 5670, 6310, 9550, 8700, 9130, 9160,
-#'     8490, 6400, 4420, 3680, 3190, 3960, 3290, 4220, 4220)),
-#'   .Names = c("yr", "ct"),
-#'   class = "data.frame",
-#'   row.names = 1:33)
-#' x <- cmsy(d$yr, ct = d$ct, prior_log_mean = 0.035, prior_log_sd = 0.68)
-#' head(x)
-#' par(mfrow = c(1, 3))
-#' plot(d, type = "o", xlab = "Year", ylab = "Catch (t)")
-#' plot(density(x$J))
-#' plot(x$r, x$k)
+#' x <- cmsy(blue_gren$yr, ct = blue_gren$ct, prior_log_mean = 0.035,
+#'   prior_log_sd = 0.68)
+#' head(x$shaefer)
+#' par(mfrow = c(2, 2))
+#' plot(blue_gren$yr, blue_gren$ct, type = "o", xlab = "Year", ylab = "Catch (t)")
+#' plot(blue_gren$yr,  apply(x$biomass, 1, median)[-1], type = "o",
+#'   ylab = "Estimated biomass", xlab = "Year")
+#' hist(x$shaefer$bmsy)
+#' plot(x$shaefer$r, x$shaefer$k)
+
 cmsy <- function(
   yr,
   ct,
