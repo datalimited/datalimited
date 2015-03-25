a5ec7381318720b7a558dfc86b9784515b101b9d
Sean Anderson <sean@seananderson.ca>
2015-03-25 15:47:52 -0700

    Add example data from RAM database

---
+++ data-raw/blue_gren.R
@@ -0,0 +1,10 @@
+# An example using cmsy() with a stock from the RAM Legacy database:
+# The stock is "BGRDRSE" (Blue Grenadier Southeast Australia)
+blue_gren <- structure(list(
+  yr = 1979:2011,
+  ct = c(512, 865, 511, 829, 935, 1390, 1260, 2240, 3370, 2770, 3050, 3290,
+    4540, 3300, 3500, 3190, 2880, 3490, 5670, 6310, 9550, 8700, 9130, 9160,
+    8490, 6400, 4420, 3680, 3190, 3960, 3290, 4220, 4220)),
+  .Names = c("yr", "ct"),
+  class = "data.frame",
+  row.names = 1:33)
