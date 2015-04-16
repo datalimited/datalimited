da316db7232cf48bd4bd7957bd91a076e8126bc5
Sean Anderson <sean@seananderson.ca>
2015-04-16 16:57:49 -0700

    Add COMSIR back into Shiny app with tab

--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -4,10 +4,12 @@ library("dplyr")
 source("helper.R")
 
 shinyUI(fluidPage(
-  titlePanel("Catch-MSY"),
+  titlePanel("Data-limited assessments"),
 
   sidebarLayout(
     sidebarPanel(
+
+      h2("General options"),
 
       selectInput(inputId = "stock",
         label = "Stock",
@@ -18,17 +20,19 @@ shinyUI(fluidPage(
         label = "Resilience",
         choices = c("Unknown", "Very low", "Low", "Medium", "High")),
 
+      h2("Catch-MSY options"),
+
       sliderInput("sig_r",
         label = "Recruitment variability",
         min = 0,
         max = 0.8,
         value = 0.05),
 
-      sliderInput("reps",
+      sliderInput("cmsy_reps",
         label = "Number of samples",
         min = 500,
         max = 20000,
-        value = 3000,
+        value = 2000,
         step = 500),
 
       sliderInput("interyr_index",
@@ -58,23 +62,92 @@ shinyUI(fluidPage(
          label = "SD depletion prior",
          min = 0.5,
          max = 4,
-         value = 2.5)
+         value = 2.5),
 
-#       checkboxGroupInput("status",
-#         label = "Choose IUCN statuses",
-#         choices = statuses,
-#         selected = statuses),
-#
-#       checkboxGroupInput("habitat",
-#         label = "Choose habitats",
-#         choices = habitats,
-#         selected = habitats)
+      h2("COM-SIR options"),
+
+       sliderInput("comsir_a",
+         label = "Fraction of K at bioeconomic equilibrium",
+         min = 0.05,
+         max = 1.0,
+         value = 0.8),
+
+       sliderInput("comsir_k",
+         label = "Stock size at carrying capacity",
+         min = 10,
+         max = 3000,
+         value = 800),
+
+       sliderInput("comsir_x",
+         label = "Intrinsic rate of increase in effort",
+         min = 0.05,
+         max = 1.0,
+         value = 0.5),
+
+       sliderInput("comsir_cv",
+         label = "CV",
+         min = 0.05,
+         max = 2.0,
+         value = 0.4),
+
+      sliderInput("comsir_reps",
+        label = "Number of initial samples",
+        min = 1000,
+        max = 100000,
+        value = 50000,
+        step = 2000),
+
+      sliderInput("comsir_n_posterior",
+        label = "Number of posterior samples",
+        min = 500,
+        max = 20000,
+        value = 5000,
+        step = 500),
+
+       checkboxInput("comsir_logk",
+         label = "Log K",
+         value = TRUE),
+
+       checkboxInput("comsir_norm_r",
+         label = "Normal r",
+         value = FALSE),
+
+       checkboxInput("comsir_norm_k",
+         label = "Normal k",
+         value = FALSE),
+
+       checkboxInput("comsir_norm_x",
+         label = "Normal x",
+         value = FALSE),
+
+       checkboxInput("comsir_norm_a",
+         label = "Normal a",
+         value = FALSE),
+
+       checkboxInput("comsir_normal_like",
+         label = "Normal likelihood",
+         value = FALSE),
+
+       checkboxInput("comsir_logistic",
+         label = "Logistic",
+         value = TRUE),
+
+       checkboxInput("comsir_obs",
+         label = "Obs??",
+         value = FALSE)
+
 
     ),
 
     mainPanel(
-      # plotOutput("plot_comsir"),
-      plotOutput("plot_cmsy")
+
+
+
+      tabsetPanel(
+        tabPanel("Catch-MSY", plotOutput("plot_cmsy")),
+        tabPanel("COMSIR", plotOutput("plot_comsir"))
+      )
+
     )
   )
 ))
