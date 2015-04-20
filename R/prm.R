72fc1310b93db0d79b5435bf79f7dd0352ff486a
Sean Anderson <sean@seananderson.ca>
2015-04-20 12:05:30 -0700

    Re-organize PRM help file into a single file

--- R/prm.R
+++ R/prm.R
@@ -1,32 +1,53 @@
-#' Format a series of predictors for a panel regression
+#' Panel regression model (Costello et al.-style model)
 #'
-#' @param year A numeric vector of years
-#' @param catch A numeric vector of catches
-#' @param bbmsy A numeric vector of B/B_MSY
-# @param von_bert_k A single value (or a numeric vector) of Von Bertalanffy
-#   growth rate parameter
-# @param temperature A single value (or a numeric vector) of the mean preferred
-#   temperature of the species
-# @param max_length A single value (or a numeric vector) of maximum recorded
-#   length for the species
-# @param age_maturity A single value (or a numeric vector) of age at which 50%
-#   of the individuals are sexually mature for the species
-#' @param species_cat A single character value of a
-#'   species category. Can be from any set of species categories as long as the
-#'   same set are used in model fitting and extrapolation.
-#' @export
+#' Format for, fit, and predict from a panel regression model. The
+#' \pkg{datalimited} package includes cached data that can be used with
+#' \code{fit_prm} and a cached model that can be used with \code{predict_prm}.
+#' These functions can also be used with new data.
+#'
+#' @name prm
 #' @references Costello, C., D. Ovando, R. Hilborn, S. D. Gaines, O. Deschenes,
 #' and S. E. Lester. 2012. Status and Solutions for the World's Unassessed
 #' Fisheries. Science 338:517-520.
-#' @return A data frame formatted for use with \code{\link{fit_prm}} or
-#'   \code{\link{predict_prm}}
-#' @family prm
 #' @examples
+#' # format_prm()
 #' d <- dplyr::inner_join(ramts, spp_categories, by = "scientificname")
 #' ram_prm_dat <- plyr::ddply(d, "stockid", function(x) {
 #'   format_prm(year = x$year, catch = x$catch, bbmsy = x$bbmsy_ram,
 #'     species_cat = x$spp_category[1L])
 #' })
+#'
+#' # fit_prm()
+#' m <- fit_prm(ram_prm_dat)
+#'
+#' # predict_prm()
+#' d <- subset(ram_prm_dat, stockid == "BGRDRSE")
+#' x <- predict_prm(d, model = m)
+#' plot(x)
+#'
+# with confidence intervals:
+#' library("ggplot2")
+#' x <- predict_prm(d, ci = TRUE)
+#' x$year <- rev(d$years_back)
+#' ggplot(x, aes(year, fit)) +
+#'   geom_ribbon(aes(ymin = lower, ymax = upper), fill = "#00000040") +
+#'   geom_line() +
+#'   ylab(expression(B/B[MSY]))
+NULL
+
+#' @description \code{format_prm}: Format a series of predictors for a panel
+#'   regression
+#'
+#' @param year A numeric vector of years
+#' @param catch A numeric vector of catches
+#' @param bbmsy A numeric vector of B/B_MSY
+#' @param species_cat A single character value of a
+#'   species category. Can be from any set of species categories as long as the
+#'   same set are used in model fitting and extrapolation.
+#' @export
+#' @return \code{format_prm}: A data frame formatted for use with \code{fit_prm} or
+#'   \code{predict_prm}
+#' @rdname prm
 
 format_prm <- function(year, catch, bbmsy, species_cat) {
 
@@ -57,18 +78,12 @@ format_prm <- function(year, catch, bbmsy, species_cat) {
     species_cat = rep(species_cat, .n))
 }
 
-#' Fit panel regression
+#' @description \code{fit_prm}: Fit a panel regression
 #'
 #' @param dat A data frame created by \code{\link{format_prm}}
 #' @export
-#' @return A linear model
-#' @seealso \code{\link{format_prm}}, \code{\link{predict_prm}}
-#' @references Costello, C., D. Ovando, R. Hilborn, S. D. Gaines, O. Deschenes,
-#' and S. E. Lester. 2012. Status and Solutions for the World's Unassessed
-#' Fisheries. Science 338:517-520.
-#' @family prm
-#' @examples
-#' m <- fit_prm(ram_prm_dat)
+#' @return \code{fit_prm}: A linear model for use with \code{predict_prm}
+#' @rdname prm
 
 fit_prm <- function(dat) {
   lm(log(bbmsy) ~
@@ -87,7 +102,7 @@ fit_prm <- function(dat) {
     data = dat)
 }
 
-#' Predict from a panel regression model
+#' \code{predict_prm}: Predict from a panel regression model
 #'
 #' @param newdata A data frame to predict on that has been formatted with
 #'   \code{\link{format_prm}}.
@@ -96,29 +111,14 @@ fit_prm <- function(dat) {
 #'   \code{\link{ram_prm_model}}.
 #' @param ci Should confidence intervals on B/B_MSY be returned?
 #' @param level Confidence interval level
-#' @return If \code{ci = FALSE}, a vector of predictions of B/B_MSY. If \code{ci
-#'   = TRUE}, a data frame with columns for B/B_MSY \code{fit} and lower and
-#'   upper confidence intervals \code{lower}, \code{upper}. Note that the model
-#'   is fitted to log(B/B_MSY) and the output from \code{predict_prm} is
-#'   exponentiated so the prediction represents an estimate of median B/B_MSY.
+#' @return \code{predict_prm}: If \code{ci = FALSE}, a vector of predictions of
+#'   B/B_MSY. If \code{ci = TRUE}, a data frame with columns for B/B_MSY
+#'   \code{fit} and lower and upper confidence intervals \code{lower},
+#'   \code{upper}. Note that the model is fitted to log(B/B_MSY) and the output
+#'   from \code{predict_prm} is exponentiated so the prediction represents an
+#'   estimate of median B/B_MSY.
 #' @export
-#' @references Costello, C., D. Ovando, R. Hilborn, S. D. Gaines, O. Deschenes,
-#' and S. E. Lester. 2012. Status and Solutions for the World's Unassessed
-#' Fisheries. Science 338:517-520.
-#' @family prm
-#' @examples
-#' d <- subset(ram_prm_dat, stockid == "BGRDRSE")
-#' x <- predict_prm(d)
-#' plot(x)
-#'
-# with confidence intervals:
-#' library("ggplot2")
-#' x <- predict_prm(d, ci = TRUE)
-#' x$year <- rev(d$years_back)
-#' ggplot(x, aes(year, fit)) +
-#'   geom_ribbon(aes(ymin = lower, ymax = upper), fill = "#00000040") +
-#'   geom_line() +
-#'   ylab(expression(B/B[MSY]))
+#' @rdname prm
 
 predict_prm <- function(newdata, model = datalimited::ram_prm_model,
   ci = FALSE, level = 0.95) {
