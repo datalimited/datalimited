c23bc4f39f1c3637c53d463ecd0bfe1faa659611
Sean Anderson <sean@seananderson.ca>
2015-09-04 14:05:15 -0700

    Standardize outputs and inputs
    Every method now has $bbmsy as an output element.

--- R/prm.R
+++ R/prm.R
@@ -1,4 +1,4 @@
-#' Panel regression model (Costello et al.-style model)
+#' Modified panel regression model
 #'
 #' Format for, fit, and predict from a panel regression model. The
 #' \pkg{datalimited} package includes cached data that can be used with
@@ -10,17 +10,20 @@
 #' and S. E. Lester. 2012. Status and Solutions for the World's Unassessed
 #' Fisheries. Science 338:517-520.
 #' @examples
-#' # format_prm()
+#' # combine two built in datasets:
 #' d <- dplyr::inner_join(ram_ts, spp_categories, by = "scientificname")
-#' ram_prm_dat <- plyr::ddply(d, "stockid", function(x) {
-#'   format_prm(year = x$year, catch = x$catch, bbmsy = x$bbmsy_ram,
-#'     species_cat = x$spp_category[1L])
-#' })
 #'
-#' # fit_prm()
+#' # ram_prm_dat is built into the package; it can be created with:
+#' # ram_prm_dat <- plyr::ddply(d, "stockid", function(x) {
+#' #   format_prm(year = x$year, catch = x$catch, bbmsy = x$bbmsy_ram,
+#' #     species_cat = x$spp_category[1L])
+#' # })
+#'
+#' # ram_prm_model is built into the package, it is created in the same manner
+#' # as this:
 #' m <- fit_prm(ram_prm_dat)
 #'
-#' # predict_prm()
+#' # now use the model to predict B/Bmsy:
 #' d <- subset(ram_prm_dat, stockid == "BGRDRSE")
 #' x <- predict_prm(d, model = m)
 #' plot(x)
@@ -28,9 +31,8 @@
 #' # with confidence intervals:
 #' library("ggplot2")
 #' x <- predict_prm(d, ci = TRUE)
-#' x$year <- rev(d$years_back)
-#' ggplot(x, aes(year, fit)) +
-#'   geom_ribbon(aes(ymin = lower, ymax = upper), fill = "#00000040") +
+#' ggplot(x$bbmsy, aes(year, bbmsy_q50)) +
+#'   geom_ribbon(aes(ymin = bbmsy_q2.5, ymax = bbmsy_q97.5), fill = "#00000040") +
 #'   geom_line() +
 #'   ylab(expression(B/B[MSY]))
 NULL
@@ -57,10 +59,10 @@ format_prm <- function(year, catch, bbmsy, species_cat) {
   .n <- length(catch)
   max_catch <- max(catch)
   scaled_catch <- catch / max_catch
-  scaled_catch1 <- lag(scaled_catch, n = 1L)
-  scaled_catch2 <- lag(scaled_catch, n = 2L)
-  scaled_catch3 <- lag(scaled_catch, n = 3L)
-  scaled_catch4 <- lag(scaled_catch, n = 4L)
+  scaled_catch1 <- as.numeric(lag(scaled_catch, n = 1L))
+  scaled_catch2 <- as.numeric(lag(scaled_catch, n = 2L))
+  scaled_catch3 <- as.numeric(lag(scaled_catch, n = 3L))
+  scaled_catch4 <- as.numeric(lag(scaled_catch, n = 4L))
   catch_to_rolling_max <- scaled_catch / cummax(scaled_catch)
   mean_scaled_catch <- mean(scaled_catch)
   time_to_max <- min(seq_along(catch)[catch == max(catch)])
@@ -127,6 +129,7 @@ fit_prm <- function(dat,
 #' @export
 #' @rdname prm
 
+
 predict_prm <- function(newdata, model = datalimited::ram_prm_model,
   ci = FALSE, level = 0.95) {
 
@@ -138,11 +141,29 @@ predict_prm <- function(newdata, model = datalimited::ram_prm_model,
   }
 
   if (!ci) {
-    return(exp(p))
+    out <- exp(p)
   } else {
-    q <- -qnorm((1 - level)/2)
-    data.frame(fit = exp(p$fit),
-      lower = exp(p$fit - q * p$se.fit),
-      upper = exp(p$fit + q * p$se.fit))
+    out_main <- prm_ci(p, level = level)
+
+    out1 <- prm_ci(p, level = 0.95)
+    out2 <- prm_ci(p, level = 0.50)
+    out1 <- dplyr::rename(out1, bbmsy_q2.5 = lower, bbmsy_q50 = fit, bbmsy_q97.5 = upper)
+    out2 <- dplyr::rename(out2, bbmsy_q25 = lower, bbmsy_q75 = upper)
+    out2$fit <- NULL
+    out1$median <- out1$fit
+    bbmsy <- data.frame(year = newdata$year, catch = newdata$catch, out1, out2)
+    bbmsy <- bbmsy[,c("year", "catch", "bbmsy_q2.5", "bbmsy_q25", "bbmsy_q50",
+      "bbmsy_q75", "bbmsy_q97.5")]
+    out <- list()
+    out$prm <- out_main
+    out$bbmsy <- bbmsy
   }
+  out
+}
+
+prm_ci <- function(p, level = 0.95) {
+  q <- -qnorm((1 - level)/2)
+  data.frame(fit = exp(p$fit),
+    lower = exp(p$fit - q * p$se.fit),
+    upper = exp(p$fit + q * p$se.fit))
 }
